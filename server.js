4a0f9a9d3b59be76fd8b75813a87ab094f566ee2
marcelo <muniz_marcelo@hotmail.com>
2018-11-25 13:00:56 -0300

    teste auth

--- server.js
+++ server.js
@@ -1,25 +1,48 @@
 require('./config/config');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 var app = express();
+app.use(bodyParser.json());
+
 const port = process.env.PORT;
 
-const configTest = process.env.TEST
+var admin = require("firebase-admin");
 
-app.get('/login', (req, res) => {
+admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: 'bestworld-eaa92',
+      clientEmail: process.env.CLIENT_EMAIL,
+      privateKey: process.env.PRIVATE_KEY
+    }),
+    databaseURL: "https://bestworld-eaa92.firebaseio.com"
+  });
+
+app.post('/login', (req, res) => {
+
+    let adminStatus = false;
+    admin.auth().getUserByEmail('mmunizs1975@gmail.com').then((userAuth) => {
+        const currentCustomClaims = userAuth.customClaims;
+        if (currentCustomClaims.admin) {
+            adminStatus = true
+        }
+    
+      }).catch((error) => {
+        console.log(`${error.code}:${error.message}`);
+      });
+
+    console.log(req.body);
+
     const user = {
         token: "123456789",
-        admin: true,
-        config: configTest
+        admin: adminStatus
     }
     res.send(user);
   }, (e) => {
     res.status(400).send(e);
 });
 
-
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
 });
-
