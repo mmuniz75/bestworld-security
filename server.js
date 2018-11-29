6a7c851965fa5d29f222adeb1ec527d50092fb4d
marcelo <muniz_marcelo@hotmail.com>
2018-11-29 16:11:19 -0300

    login MS

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('./config/config');
 
 const express = require('express');
+const axios = require('axios');
 const bodyParser = require('body-parser');
 
 var app = express();
@@ -10,15 +11,40 @@ const port = process.env.PORT;
 
 app.post('/login', (req, res) => {
 
-    let adminStatus = false;
-    console.log(req.body);
+  const authData = {
+    email: req.body.user,
+    password: req.body.password,
+    returnSecureToken: true
+  };
 
-    const user = {
-        token: "123456789",
-        admin: adminStatus
+  let user = {};
+   
+  axios.post(process.env.AUTH_URL,authData).then((response) => {
+  
+    const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+
+    user = {
+      token: response.data.idToken,
+      id:response.data.localId,
+      expirationDate: expirationDate,
+      role: "default"
     }
+
+    return axios.get(`${process.env.FIREBASE_SERVER}/usuarios.json?orderBy="email"&equalTo="${authData.email}"&auth=${user.token}`);
+
+  }).then((response) => {  
+    Object.keys(response.data).map(key => {
+      const userData = response.data[key];
+      user.role = userData.role;
+    });
+
     res.send(user);
-  }, (e) => {
+        
+  }).catch((e) => {
+      res.status(e.response.status).send(e.response.data);
+  });
+
+}, (e) => {
     res.status(400).send(e);
 });
 
