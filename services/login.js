b881d5aa8f5ae776886acd1fab525a20c6d0a340
marcelo <muniz_marcelo@hotmail.com>
2018-12-20 14:15:06 -0200

    verifica usuario desativado

--- services/login.js
+++ services/login.js
@@ -9,10 +9,17 @@ const login = async (email,password) => {
       
       user.role = await userUtil.getRole(email,user.token);
 
+      if(user.role === 'disable') {
+          throw new Error("USER_DISABLED")
+      }
       return {"user": user};
           
     }catch(e) {
-        const message = e.response.data.error.message?e.response.data.error.message:e.response.data.error;
+        let message = e.message;
+        
+        if(e.response) {
+            message = e.response.data.error.message?e.response.data.error.message:e.response.data.error;
+        }    
         throw new Error(message);
     };
 
