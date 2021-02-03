b0b895b00717a42735a4815067e9cfba5d3a4685
mmuniz75 <mmunizs1975@gmail.com>
2021-02-03 16:01:34 -0300

    parametros para https

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use(bodyParser.json());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json,access-token');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json,access-token,Access-Control-Request-Method, Access-Control-Request-Headers');
   next();
 });
 
